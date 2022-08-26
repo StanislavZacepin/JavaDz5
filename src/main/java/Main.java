e51633cc32448365636db50c29a218a143fa9161
†¤¤† †¤¤† <yaolsher@yandex.ru>
2022-08-26 17:52:00 +0300

    Создали функцыю. Для подщёта дублированных значений

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.HashMap;
 
 public class Main {
@@ -10,9 +11,49 @@ public class Main {
         for (int i = 0; i < 10; i++) {
             Array.put(i, 0);
         }
+        Array = Count(Array, nums1);
+        Array = Count(Array, nums2);
 
 
         return;
     }
-}
+
+    public HashMap<Integer, Integer> Count(HashMap<Integer, Integer> Array, int[] nums) {
+        for (int i : nums) {
+            switch (i) {
+                case (0):
+                    Array.put(0, ++);
+                    break;
+                case (1):
+                    Array.put(1, ++);
+                    break;
+                case (2):
+                    Array.put(2, ++);
+                    break;
+                case (3):
+                    Array.put(3, ++);
+                    break;
+                case (4):
+                    Array.put(4, ++);
+                    break;
+                case (5):
+                    Array.put(5, ++);
+                    break;
+                case (6):
+                    Array.put(6, ++);
+                    break;
+                case (7):
+                    Array.put(7, ++);
+                    break;
+                case (8):
+                    Array.put(8, ++);
+                    break;
+                case (9):
+                    Array.put(0, ++);
+                    break;
+
+            }
+        }
+        return Array;
+    }
 

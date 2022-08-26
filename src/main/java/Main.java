7a97700919f6bfc398a057dd1d1ad93e057e044a
†¤¤† †¤¤† <yaolsher@yandex.ru>
2022-08-26 16:21:37 +0300

    Подготовка тестовых данных

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,12 @@
+import java.util.HashMap;
+
 public class Main {
     public static void main(String[] args) {
-        boolean isFormatted = false;
-        String request = "пожалуйста форматируйте код";
-        // исправили теперь build succcessful
-        if (isFormatted) {
-        }
+        int[] nums1 = new int[]{1, 2, 2, 1}, nums2 = new int[]{2, 2};
+    }
+
+    public int[] intersect(int[] nums1, int[] nums2) {
+
+        return;
     }
 }

35c7c2d997fef370e6a5603ef82455645a74952a
†¤¤† †¤¤† <yaolsher@yandex.ru>
2022-08-26 18:45:57 +0300

    Решил делать через список ArrayList

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,71 +1,37 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) {
         int[] nums1 = new int[]{1, 2, 2, 1}, nums2 = new int[]{2, 2};
-         int[] num3 = intersect(nums1,nums2);
+        int[] num3 = intersect(nums1, nums2);
     }
 
     public static int[] intersect(int[] nums1, int[] nums2) {
-        HashMap<Integer, Integer> Array = new HashMap<>(); // Ключ = число, значения = количестов этого числа
-        for (int i = 0; i < 10; i++) {
-            Array.put(i, 0);
-        }
-        int count = 0;
-        Counts(Array, nums1);
-        Counts(Array, nums2);
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        ArrayList<Integer> s = new ArrayList<Integer>();
+        int i = 0, j = 0;
 
+        while ((i < nums1.length) && (j < nums2.length))
 
-        return null;
-    }
+            if (nums1[i] < nums2[j]) i++;
 
-    public static void Counts(HashMap<Integer, Integer> Array, int[] nums) {
-        int value = 0;
-        for (int i : nums) {
-            switch (i) {
-                case (0):
-                    value =  Array.get(i);
-                    Array.put(0, value+1);
-                    break;
-                case (1):
-                    value =  Array.get(i);
-                    Array.put(1, value+1);
-                    break;
-                case (2):
-                    value =  Array.get(i);
-                    Array.put(2, value+1);
-                    break;
-                case (3):
-                    value =  Array.get(i);
-                    Array.put(3, value+1);
-                    break;
-                case (4):
-                    value =  Array.get(i);
-                    Array.put(4, value+1);
-                    break;
-                case (5):
-                    value =  Array.get(i);
-                    Array.put(5, value+1);
-                    break;
-                case (6):
-                    value =  Array.get(i);
-                    Array.put(6, value+1);
-                    break;
-                case (7):
-                    value =  Array.get(i);
-                    Array.put(7, value+1);
-                    break;
-                case (8):
-                    value =  Array.get(i);
-                    Array.put(8, value+1);
-                    break;
-                case (9):
-                    value =  Array.get(i);
-                    Array.put(9, value+1);
-                    break;
+            else if (nums1[i] > nums2[j]) j++;
 
+            else {
+                s.add(nums1[i]);
+                i++;
+                j++;
             }
-        }
 
+        int res[] = new int[s.size()];
+
+        int k = 0;
+
+        for (Integer val : s) res[k++] = val;
+
+        return res;
     }
 }

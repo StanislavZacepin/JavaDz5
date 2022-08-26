e845df382a97c25a8512352e59b9f9908e85e6c1
†¤¤† †¤¤† <yaolsher@yandex.ru>
2022-08-26 19:35:05 +0300

    Рефакторинг

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,36 +1,42 @@
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) {
-        int[] nums1 = new int[]{1, 2, 2, 1}, nums2 = new int[]{2, 2};
-        int[] num3 = intersect(nums1, nums2);
+
+        int[] nums1 = new int[]{1, 2, 2, 1};
+        int[] nums2 = new int[]{2, 2};
+
+        intersect(nums1, nums2);
     }
 
     public static int[] intersect(int[] nums1, int[] nums2) {
         Arrays.sort(nums1);
         Arrays.sort(nums2);
-        ArrayList<Integer> s = new ArrayList<Integer>();
-        int i = 0, j = 0;
+        ArrayList<Integer> s = new ArrayList<>();
+        int i = 0;
+        int j = 0;
 
-        while ((i < nums1.length) && (j < nums2.length))
+        while ((i < nums1.length) && (j < nums2.length)) {
 
-            if (nums1[i] < nums2[j]) i++;
-
-            else if (nums1[i] > nums2[j]) j++;
-
-            else {
+            if (nums1[i] < nums2[j]) {
+                i++;
+            } else if (nums1[i] > nums2[j]) {
+                j++;
+            } else {
                 s.add(nums1[i]);
                 i++;
                 j++;
             }
+        }
 
-        int res[] = new int[s.size()];
+        int[] res = new int[s.size()];
 
         int k = 0;
 
-        for (Integer val : s) res[k++] = val;
+        for (Integer val : s) {
+            res[k++] = val;
+        }
 
         return res;
     }
